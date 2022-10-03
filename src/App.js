fb0e38629394686e62acd1718f8ed54f94a3380e
Anis <imarahmandev@gmail.com>
2022-10-03 15:57:12 +0700

    adding mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p className='text-3xl font-bold underline'>React App</p>
+        <Button variant='contained'>Hello</Button>
       </header>
     </div>
   );
