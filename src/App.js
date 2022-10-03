c0ee485f96ac9c610eb845a950fa1368664daf4f
Anis <imarahmandev@gmail.com>
2022-10-03 15:31:31 +0700

    tailwind css integration

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>React App</p>
+        <p className='text-3xl font-bold underline'>React App</p>
       </header>
     </div>
   );
